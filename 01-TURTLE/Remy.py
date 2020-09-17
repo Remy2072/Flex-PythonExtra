0aeb019db1bc51c311c6340072db8fc7185c533b
Remy2072 <30528@ma-web.nl>
2020-09-17 14:33:00 +0200

    README gewijzigd

---
+++ 01-TURTLE/Remy.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.left(90)
+turtle.forward(150)
+turtle.right(90)
+turtle.forward(80)
+turtle.right(90)
+turtle.forward(85)
+turtle.right(90)
+turtle.forward(80)
+turtle.right(220)
+turtle.forward(105)
+turtle.done()
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Flex-PythonExtra
-Flex Python Extra opdrachten van Remy Duivesteijn klas: SD1A
+Flex Python Extra opdrachten van Remy Duivesteijn - SD1A
